a22423c8fb65b770541b91990badaa66f3071edf
alhasann351 <alhasann352@gmail.com>
2024-03-14 21:49:13 +0600

    fittedbox apply in column

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    DevicePreview(builder: (context) => const MyApp()),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -11,6 +16,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+      darkTheme: ThemeData.dark(),
       theme: ThemeData(
         primaryColor: Colors.blue,
       ),
@@ -54,7 +62,7 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ),
       ),*/
-      body: FittedBox(
+      /*body: FittedBox(
         child: Row(
           children: [
             Container(
@@ -66,6 +74,53 @@ class _MyHomePageState extends State<MyHomePage> {
               height: 200,
               width: 400,
               color: Colors.green,
+            ),
+          ],
+        ),
+      ),*/
+      body: FittedBox(
+        child: Column(
+          children: [
+            FittedBox(
+              child: Container(
+                height: 200,
+                width: 400,
+                color: Colors.red,
+              ),
+            ),
+            FittedBox(
+              child: Container(
+                height: 200,
+                width: 400,
+                color: Colors.green,
+              ),
+            ),
+            const FittedBox(
+              child: Text(
+                'This is the FittedBox Widget in Flutter, This app is linked to the debug service',
+                maxLines: 1,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 25,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            FittedBox(
+              child: Row(
+                children: [
+                  Container(
+                    height: 200,
+                    width: 400,
+                    color: Colors.red,
+                  ),
+                  Container(
+                    height: 200,
+                    width: 400,
+                    color: Colors.green,
+                  ),
+                ],
+              ),
             ),
           ],
         ),
